c14c2662466fa353d26ac53fe86c325fa19748af
Taufik Fredi Pratama <taufik@jayantara.co.id>
2025-07-02 14:36:41 +0700

    Tambahkan Month and Year Picker

--- Hanggar/src/main/java/id/jagr/mod/hanggar/Constants.kt
+++ Hanggar/src/main/java/id/jagr/mod/hanggar/Constants.kt
@@ -2,4 +2,7 @@ package id.jagr.mod.hanggar
 
 const val DATETIME_FORMAT: String = "yyyy-MM-dd HH:mm:ss"
 const val DATE_FORMAT: String = "yyyy-MM-dd"
+const val DAY_FORMAT: String = "dd"
+const val MONTH_FORMAT: String = "MM"
+const val YEAR_FORMAT: String = "yyyy"
 const val TIME_FORMAT: String = "HH:mm:ss"
